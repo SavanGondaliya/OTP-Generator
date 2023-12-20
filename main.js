ffaf32d74cbb9db773c8f6b5938753621946e617
Savan <savangondaliya0@gmail.com>
2023-12-20 21:30:32 +0530

    All Most Done

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
-function Generate_otp(){
-    let number = '1234567890';
-    let length = 4;
+function Parent(){
+    Generate_otp(4);
+    ChangeOver();
+}
+
+function Generate_otp(length){
+    let otp = '';
     for(let i=0;i<length;i++){
-    let otp = Math.floor(Math.random()*10);
-    document.getElementById('OTP').innerHTML = otp+=;
+        let number = Math.floor(Math.random()*10);
+        otp += number;
+    }
+    document.getElementById('OTP').innerHTML = otp;
+}
+function ChangeOver()
+{
+    document.getElementById('form').style.display = 'none';
+    document.getElementById('Verifier').style.display  = 'flex';
+    document.getElementById('display').style.display = 'block';
+}
+// Verification 
+
+function Verify()
+{
+    let OTP = document.getElementById('OTP').value;
+    let confirm = document.getElementById('confirm').value;
+    
+    if(OTP === confirm)
+    {
+        document.getElementById('Login_message').style.display = 'block';
+        document.getElementById('Verifier').style.display = 'none';
+        document.getElementById('display').style.display = 'none';
+    }
+    else
+    {
+        return false;       
     }
 }
