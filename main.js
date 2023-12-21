a8ce8030cc38dd0231f2e5a773aae47d571d7220
Savan <savangondaliya0@gmail.com>
2023-12-21 09:51:13 +0530

    Clean UnNecesarry Code

--- main.js
+++ main.js
@@ -1,36 +1,31 @@
-function Parent(){
-    Generate_otp(4);
-    ChangeOver();
+function Parent() {
+  Generate_otp(4);
+  ChangeOver();
 }
 
-let otp = '';
-function Generate_otp(length){
-    for(let i=0;i<length;i++){
-        let number = Math.floor(Math.random()*10);
-        otp += number;
-    }
-    document.getElementById('OTP').innerHTML = otp;
+let otp = "";
+function Generate_otp(length) {
+  for (let i = 0; i < length; i++) {
+    let number = Math.floor(Math.random() * 10);
+    otp += number;
+  }
+  document.getElementById("OTP").innerHTML = otp;
 }
-function ChangeOver()
-{
-    document.getElementById('form').style.display = 'none';
-    document.getElementById('Verifier').style.display  = 'flex';
-    document.getElementById('display').style.display = 'block';
+function ChangeOver() {
+  document.getElementById("form").style.display = "none";
+  document.getElementById("Verifier").style.display = "flex";
+  document.getElementById("display").style.display = "block";
 }
-// Verification 
-function Verify()
-{
-    let OTP = document.getElementById('OTP').value;
-    let confirm = document.getElementById('confirm').value;
-    
-    if(otp === confirm)
-    {
-        document.getElementById('Login_message').style.display = 'block';
-        document.getElementById('Verifier').style.display = 'none';
-        document.getElementById('display').style.display = 'none';
-    }
-    else
-    {
-        return false;
-    }
-}
+// Verification
+function Verify() {
+  let OTP = document.getElementById("OTP").value;
+  let confirm = document.getElementById("confirm").value;
+
+  if (otp === confirm) {
+    document.getElementById("Login_message").style.display = "block";
+    document.getElementById("Verifier").style.display = "none";
+    document.getElementById("display").style.display = "none";
+  } else {
+    return false;
+  }
+~}
